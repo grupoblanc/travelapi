fe5b4a7d6bf59c886a80d442b5622eaeb7496ef4
Christopher Ventura <venturachrisdev@gmail.com>
2017-10-02 09:35:50 -0400

    debug 2

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(pretty({query: 'json'}));
-app.use(logger('debug'));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
