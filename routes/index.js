2b32dc13b784a9874d702b9f9677a637f837a8ea
Christopher Ventura <venturachrisdev@gmail.com>
2017-10-06 09:42:23 -0400

    previous commit x2

--- routes/index.js
+++ routes/index.js
@@ -867,7 +867,7 @@ function ifCity(city, milis, req, res) {
 					});
 				});
 				topics = topics.filter(function(topic) {
-					return topic.length > 3
+					return topic.places.length > 3
 				});
 				res.json({
 						city: {
